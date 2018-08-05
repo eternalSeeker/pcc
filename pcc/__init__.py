c6e85946d90d16d3762c9f73d597d53e04653a41
Bart Opsomer <opsomerbart@gmail.com>
2018-08-05 17:37:16 +0200

    added document structure from github.com/seanfisk/python-project-template

---
+++ pcc/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""A python based C compiler, intended to learn how compilers work not to generate optimized binaries for C"""
+
+from pcc import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ pcc/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'pcc'
+project = "pcc"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'A python based C compiler, intended to learn how compilers work not to generate optimized binaries for C'
+authors = ['Bart Opsomer']
+authors_string = ', '.join(authors)
+emails = ['opsomerbart@gmail.com']
+license = 'MIT'
+copyright = '2018 ' + authors_string
+url = 'https://github.com/eternalSeeker/pcc'
