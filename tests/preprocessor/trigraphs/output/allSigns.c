5e5db5fa9b1f64f15ea4d9da33589986a19579e3
Bart Opsomer <opsomerbart@gmail.com>
2018-08-12 17:40:26 +0200

    added more realistic C test file using trigraphs

---
+++ tests/preprocessor/trigraphs/output/allSigns.c
@@ -0,0 +1,8 @@
+/*this file contains a more realistic piece of C code using all trigraphs*/
+#include "helpMe.h"
+#define A_MULTI_LINE_DEFINE \
+    (0x42)
+int a = 0xde ^ 0xad;
+int b = ~(A_MULTI_LINE_DEFINE);
+char config[3] = {1, 2, 3};
+int c = 8|2;
